cba06b3c23cd1f630cd7bdcd5092fde5ec6855f2
James Davey <james.t.davey@gmail.com>
2019-02-03 21:19:38 -0800

    fix center alignment of contact button

--- src/components/layout/ContactIcon/ContactIcon.js
+++ src/components/layout/ContactIcon/ContactIcon.js
@@ -1,18 +1,11 @@
 import React from 'react';
-import ImageFadeIn from 'react-image-fade-in';
 
 import classes from './ContactIcon.module.css';
 
-import envelope from './../../../data/images/icons/envelope.svg';
-
-const ContactIcon = () => {
-  return (
-    <a href="mailto:james.t.davey@gmail.com" className={classes.wrapper}>
-      <div className={classes.wrapper__inner}>
-        <ImageFadeIn src={envelope} className={classes.envelope} />
-      </div>
-    </a>
-  );
-};
+const ContactIcon = () => (
+  <a href="mailto:james.t.davey@gmail.com" className={classes.wrapper}>
+    <div className={classes.envelope} />
+  </a>
+);
 
 export default ContactIcon;
